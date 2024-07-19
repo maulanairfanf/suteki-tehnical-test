5a541689f59987699144e137c88100884ab0c2d6
maulanairfanf <maulanairfanf@gmail.com>
2024-07-19 11:35:10 +0700

    add about us to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
 	<StepRegistration />
+	<AboutUs />
 </template>
 <script setup>
 import StepRegistration from '@/components/StepRegistration.vue'
+import AboutUs from '@/components/AboutUs.vue'
+
 definePageMeta({
 	title: 'My home page',
 })
