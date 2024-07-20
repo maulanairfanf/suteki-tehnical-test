d76612e91940acab0a9ea67259518fe4bba665c7
maulanairfanf <maulanairfanf@gmail.com>
2024-07-20 11:29:44 +0700

    fixing ui step registration

--- components/StepRegistration.vue
+++ components/StepRegistration.vue
@@ -1,21 +1,23 @@
 <template>
-	<div class="container step-box-background radius-12">
-		<div class="grid-4-10px" id="box-step-registration">
-			<div
-				class="grid-item-1 flex justify-start items-center p-medium"
-				v-for="(item, index) in data"
-				:key="index"
-			>
-				<div>
-					<div class="circle flex items-center justify-center font-size-16">
-						{{ index + 1 }}
+	<div class="container">
+		<div class="step-box-background radius-12 p-small">
+			<div class="grid-4-10px" id="box-step-registration">
+				<div
+					class="grid-item-1 flex justify-start items-center p-medium"
+					v-for="(item, index) in data"
+					:key="index"
+				>
+					<div>
+						<div class="circle flex items-center justify-center font-size-16">
+							{{ index + 1 }}
+						</div>
 					</div>
-				</div>
-				<div class="text-white ml-medium">
-					<div class="font-size-16">{{ item.title }}</div>
-					<div class="font-size-12 font-weight-light mt-small hide-on-mobile">
-						Consectetur adipiscing elit. Urna amet hac senectus in consequat
-						orci duis pellentesque.
+					<div class="text-white ml-medium">
+						<div class="font-size-16">{{ item.title }}</div>
+						<div class="font-size-12 font-weight-light mt-small hide-on-mobile">
+							Consectetur adipiscing elit. Urna amet hac senectus in consequat
+							orci duis pellentesque.
+						</div>
 					</div>
 				</div>
 			</div>
