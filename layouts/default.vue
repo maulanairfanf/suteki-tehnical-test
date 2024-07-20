1a1841b9a33bbabf8259a03925a51a902db2bbac
maulanairfanf <maulanairfanf@gmail.com>
2024-07-20 13:25:00 +0700

    add footer to home

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,11 @@
 	<div>
 		<Navbar />
 		<slot />
+
+		<Footer />
 	</div>
 </template>
 <script setup>
 import Navbar from '../components/Navbar.vue'
+import Footer from '../components/Footer.vue'
 </script>
