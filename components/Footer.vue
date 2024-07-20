97d6298d52df96a95ea3a24a69b020544947dad0
maulanairfanf <maulanairfanf@gmail.com>
2024-07-20 13:24:46 +0700

    made footer

---
+++ components/Footer.vue
@@ -0,0 +1,75 @@
+<template>
+	<div class="container">
+		<div
+			class="linear-background grid-4-20px text-white radius-12 hide-on-mobile"
+			style="padding: 100px"
+		>
+			<div class="grid-item-2">
+				<h1 class="font-size-18 font-weight-semi-bold">Tentang Kami</h1>
+				<p class="font-size-16 font-weight-normal">
+					Membantu siswa mencari dan membandingkan informasi perguruan tinggi di
+					Jawa Barat dan Banten agar bisa meraih impian lewat kampus idaman dan
+					jurusan yang sesuai dengan passion.
+				</p>
+			</div>
+			<div class="grid-item-1">
+				<h1 class="font-size-18 font-weight-semi-bold">Layanan</h1>
+				<p class="font-size-16 font-weight-normal">Daftar Kampus</p>
+				<p class="font-size-16 font-weight-normal">Daftar Prodi/jurusan</p>
+				<p class="font-size-16 font-weight-normal">Artikel/Berita</p>
+			</div>
+			<div class="grid-item-1">
+				<h1 class="font-size-18 font-weight-semi-bold">Kontak Kami</h1>
+				<div
+					class="font-size-16 font-weight-normal flex items-center mt-medium"
+				>
+					<div>
+						<img src="@/assets/images/email-icon.svg" />
+					</div>
+					<span class="ml-small mb-small">email@company.com</span>
+				</div>
+				<div
+					class="font-size-16 font-weight-normal flex items-center mt-medium"
+				>
+					<div>
+						<img src="@/assets/images/phone-icon.svg" />
+					</div>
+					<span class="ml-small mb-small">+62 32984343234</span>
+				</div>
+				<div>
+					<button
+						class="radius-4 flex items-center justify-center button-white mt-small"
+					>
+						<img src="@/assets/images/playstore-icon.svg" />
+						<span class="text-black ml-small font-size-12">
+							Download Aplikasi
+						</span>
+					</button>
+				</div>
+			</div>
+		</div>
+		<div class="mt-medium flex justify-between items-center hide-on-mobile">
+			<img src="@/assets/images/logo.png" alt="Logo" />
+			<p class="font-size-14 font-weight-light">
+				Copyright © 2020 Kampus Jabar Banten
+			</p>
+		</div>
+		<div class="hide-on-desktop flex flex-column items-center">
+			<p class="font-size-14 font-weight-light">
+				Copyright © 2020 Kampus Jabar Banten
+			</p>
+			<div>
+				<button
+					class="radius-4 flex items-center justify-center button mt-small"
+				>
+					<img src="@/assets/images/playstore-icon.svg" />
+					<span class="text-white ml-small font-size-12">
+						Download Aplikasi
+					</span>
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+<script></script>
+<style></style>
