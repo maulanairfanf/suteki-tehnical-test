0281f116c2008f63ab494b5f71b958fed1d97f70
maulanairfanf <maulanairfanf@gmail.com>
2024-07-20 13:25:24 +0700

    made and add prodi recommendation

---
+++ components/ProdiRecommendation.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="container">
+		<Header
+			title="Rekomendasi Jurusan/Prodi"
+			description="Memberikan sistim pelayanan untuk para calon mahasiswa Jawa Barat dan Banten"
+		/>
+		<div class="grid-4-20px">
+			<div
+				class="grid-item-1 bg-blue-smooth p-large radius-8"
+				v-for="index in 8"
+				:key="index"
+			>
+				<div class="font-size-12 flex items-center">
+					<div>
+						<div
+							class="bg-blue mr-small radius-2"
+							style="width: 10px; height: 10px"
+						></div>
+					</div>
+					<span> Seni, Desain & Musik</span>
+				</div>
+				<div class="font-size-14 mt-medium font-weight-bold">
+					Desain Komunikasi Visual
+				</div>
+			</div>
+		</div>
+		<div class="flex items-center justify-center my-medium pb-medium">
+			<hr class="hide-on-mobile horizontal-line" />
+			<button class="button-outline w-full mx-small">
+				Jurusan Lainnya (+32)
+			</button>
+			<hr class="hide-on-mobile horizontal-line" />
+		</div>
+	</div>
+</template>
+<script setup>
+import Header from './reusable/Header.vue'
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,13 @@
 	<StepRegistration />
 	<AboutUs />
 	<CampusRecommendations />
+	<ProdiRecommendation />
 </template>
 <script setup>
 import StepRegistration from '@/components/StepRegistration.vue'
 import AboutUs from '@/components/AboutUs.vue'
 import CampusRecommendations from '@/components/CampusRecommendations.vue'
+import ProdiRecommendation from '@/components/ProdiRecommendation.vue'
 
 definePageMeta({
 	title: 'My home page',
