a7e9f6cce7e0dab4fa4ba84afb16b3c7553633cb
maulanairfanf <maulanairfanf@gmail.com>
2024-07-26 08:53:08 +0700

    initiate

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-	<Banner />
+	<ul>
+		<li v-for="(item, index) in list" :key="index">
+			{{ item.name }}
+		</li>
+	</ul>
+	<button type="button" @click="handlePageChange()">nextPage</button>
+	<!-- <Banner />
 	<StepRegistration />
 	<AboutUs />
 	<CampusRecommendations />
-	<ProdiRecommendation />
+	<ProdiRecommendation /> -->
 </template>
 <script setup>
 import Banner from '@/components/Banner.vue'
@@ -15,4 +21,32 @@ import ProdiRecommendation from '@/components/ProdiRecommendation.vue'
 definePageMeta({
 	title: 'My home page',
 })
+
+const page = ref(1)
+const perPage = ref(5)
+const list = ref([])
+
+const url = computed(() => {
+	return `/api/open/studyprogram/all?page=${page.value}`
+})
+
+const { data, error, refresh } = await useFetch(url, {
+	watch: [page],
+})
+
+list.value = data.value.data
+
+const handlePageChange = () => {
+	page.value++
+}
+
+watch(
+	() => data.value,
+	newValue => {
+		if (newValue) {
+			console.log('newaValue', newValue)
+			list.value = [...list.value, ...newValue.data]
+		}
+	}
+)
 </script>
